6c568c84af5b3fa68efd190b4a0497331a5870fb
Nicholas Tietz-Sokolsky <me@ntietz.com>
2020-12-02 09:37:35 -0500

    Improved day 1 part 2

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -2,13 +2,14 @@ mod input;
 use std::collections::HashSet;
 
 pub fn part1_naive() {
-    let entries = crate::day1::input::INPUT;
+    let entries = crate::day1::input::INPUT.to_vec();
     let target = 2020;
 
-    for i in 0..entries.len() - 1 {
-        for j in i + 1..entries.len() {
-            if entries[i] + entries[j] == target {
-                println!("day1.part1 solution = {} (#naive)", entries[i] * entries[j]);
+    for (index, first) in entries.iter().enumerate() {
+        for second in entries[index + 1..].iter() {
+            if first + second == target {
+                println!("day1.part1 solution = {} (#naive)", first * second);
+                break;
             }
         }
     }
@@ -16,7 +17,7 @@ pub fn part1_naive() {
 
 pub fn part1() {
     let input = crate::day1::input::INPUT;
-    let entries: HashSet<i32> = input.iter().cloned().collect();
+    let entries: HashSet<i32> = input.iter().copied().collect();
     let target = 2020;
 
     for entry in input.iter() {
@@ -46,39 +47,16 @@ pub fn part2_naive() {
 }
 
 pub fn part2() {
-    let input = crate::day1::input::EXAMPLE_INPUT;
-    let entries: HashSet<i32> = input.iter().cloned().collect();
+    let input = crate::day1::input::INPUT.to_vec();
+    let entries: HashSet<i32> = input.iter().copied().collect();
     let target = 2020;
-    let mut partial_entries: HashSet<i32> = HashSet::new();
 
-    let mut sorted_entries = input.to_vec();
-    sorted_entries.sort();
-
-    let mut left = 0;
-    let mut right = input.len() - 1;
-
-    for entry in input.iter() {
-        partial_entries.insert(target - entry);
-    }
-    println!("{:#?}", partial_entries);
-
-    while left < right {
-        let partial = sorted_entries[left] + sorted_entries[right];
-        let candidate = target - partial;
-        println!(
-            "{} / {} -- {}, {}: {}, {}",
-            partial, candidate, left, right, sorted_entries[left], sorted_entries[right]
-        );
-
-        if candidate < sorted_entries[left] {
-            right -= 1;
-        } else if !entries.contains(&candidate) {
-            left += 1;
-        } else {
-            println!("NOOO");
-            break;
+    'outer: for (index, x) in input.iter().enumerate() {
+        for y in input[index + 1..].iter() {
+            if entries.contains(&(target - x - y)) {
+                println!("day1.part2.solution = {}", x * y * (target - x - y));
+                break 'outer;
+            }
         }
     }
-    println!("{} {}", left, right);
-    println!("{:#?}", sorted_entries);
 }
